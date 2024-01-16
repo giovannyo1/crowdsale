a45838284f94d18511cefb87c7ebcf40cad3fd00
gio <jramosneo@yahoo.com>
2024-01-15 18:38:50 -0700

    Adds smart contracts

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,9 +1,83 @@
-// SPDX-License-Identifier: Unlicense
+///SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract Crowdsale{
-	string public name = "Crowdsale";
+import "hardhat/console.sol";
+import "./Token.sol";
+ 
+
+    //address owner;: Almacena la dirección del propietario del contrato.
+    //Token public Almacena la dirección del propietario del contrato.
+    // price;: Almacena el precio de un token en ether.
+    // maxTokens;: Almacena la cantidad máxima de tokens disponibles para la venta.
+    // tokensSold;: Almacena la cantidad total de tokens vendidos.
+contract Crowdsale {
+    address owner;
+    Token public token;
+    uint256 public price;
+    uint256 public maxTokens;
+    uint256 public tokensSold;
 
 
+    //buy event se emite a tokens. Registra la cantidad de tokens comprados y la dirección del comprador.
+    // finalize Se emite al finalizar la venta, registrando la cantidad total de tokens vendidos y la cantidad de ether recaudada.
+    event Buy(uint256 amount, address buyer);
+    event Finalize(uint256 tokensSold, uint256 ethRaised);
+ 
+    //constructor Inicializa el contrato con la dirección del token, el precio de un token en ether y la cantidad máxima de tokens disponibles para la venta.
+    constructor(
+        Token _token,
+        uint256 _price,
+        uint256 _maxTokens
+    
+    ) {
+        owner = msg.sender;
+        token = _token;
+        price = _price;
+        maxTokens = _maxTokens;
+    
+    }
+    //Modifier Define un modificador que permite que solo el propietario ejecute ciertas funciones. En este caso, se utiliza para restringir el acceso a las funciones solo al propietario
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
 
+    // Buy tokens directly by sending Ether
+    // --> https://docs.soliditylang.org/en/v0.8.15/contracts.html#receive-ether-function
+    //Receive : Esta función es llamada cuando el contrato recibe ether directamente. Calcula la cantidad de tokens que el comprador obtendrá y llama a la función buyTokens
+    
+    receive() external payable {
+        uint256 amount = msg.value / price;
+        buyTokens(amount * 1e18);
+    }
+
+    //buyTokens Permite a los compradores adquirir tokens enviando ether. Verifica el monto de ether enviado, la disponibilidad de tokens y realiza la transferencia de tokens al comprador.
+    
+    function buyTokens(uint256 _amount) public payable {
+        require(msg.value == (_amount / 1e18) * price);
+        require(token.balanceOf(address(this)) >= _amount);
+        require(token.transfer(msg.sender, _amount));
+
+        tokensSold += _amount;
+
+        emit Buy(_amount, msg.sender);
+    }
+
+    // setPrice Permite al propietario del contrato cambiar el precio de un token en ether.
+    
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    // Finalize Sale: Finaliza la venta, transfiriendo los tokens restantes al propietario y enviando el ether recaudado al propietario.
+    
+    function finalize() public onlyOwner {
+        require(token.transfer(owner, token.balanceOf(address(this))));
+
+        uint256 value = address(this).balance;
+        (bool sent, ) = owner.call{value: value}("");
+        require(sent);
+
+        emit Finalize(tokensSold, value);
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,25 +4,37 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+
+ //Importa la biblioteca Hardhat para el desarrollo y prueba de contratos inteligentes.
 const hre = require("hardhat");
 
+//Define la función principal del script como una función asíncrona, ya que las operaciones de despliegue y transferencia pueden llevar tiempo.
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NAME = 'Dapp University'
+  const SYMBOL = 'DAPP'
+  const MAX_SUPPLY = '1000000'
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+// Precio de un token en ether, convertido a una representación numérica adecuada.
+  const PRICE = ethers.utils.parseUnits('0.025', 'ether')
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Deploy Token
+  const Token = await hre.ethers.getContractFactory("Token")
+  const token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  await token.deployed()
 
-  await lock.waitForDeployment();
+  console.log(`Token deployed to: ${token.address}\n`)
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Deploy Crowdsale
+  const Crowdsale = await hre.ethers.getContractFactory("Crowdsale")
+  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await crowdsale.deployed();
+
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
+
+  const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await transaction.wait()
+
+  console.log(`Tokens transferred to Crowdsale\n`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -31,3 +43,10 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+//En resumen, este script utiliza Hardhat para desplegar un contrato de token (Token)
+// y un contrato de venta de tokens (Crowdsale) en una red local de Ethereum. Luego, realiza
+// la transferencia de una cantidad específica de tokens al contrato de venta de tokens. 
+//Este script puede ser útil para simular y probar interacciones entre contratos inteligentes
+// durante el desarrollo
+
